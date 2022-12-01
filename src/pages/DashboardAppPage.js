c66ce342bc0659d9563db46026830ddb37b2a1c3
Rizaldy Imam Khadafi <63111113+rizaldyimam@users.noreply.github.com>
2022-12-02 00:12:16 +0700

    Update DashboardAppPage.js
    tambahan

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -37,9 +37,75 @@ export default function DashboardAppPage() {
       </Helmet>
 
       <Container maxWidth="xl">
-        <Typography variant="h5" gutterBottom>
-          Welcome
-        </Typography>
+        <Card>
+          <CardContent>
+            <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Typography variant="h5" gutterBottom>
+                Programming Quotes App by Kelompok 29
+              </Typography>
+            </Container>
+
+            <Grid container spacing={3}>
+              <Grid item sm="12" md="4" lg="4">
+                <Paper
+                  elevation={6}
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 3,
+                  }}
+                >
+                  <Typography variant="h6" gutterBottom>
+                    Soffan Marsus Ahmad
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    21120119130042
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item sm="12" md="4" lg="4">
+                <Paper
+                  elevation={6}
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 3,
+                  }}
+                >
+                  <Typography variant="h6" gutterBottom>
+                    Rizaldy Imam Khadafi
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    21120119140119
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item sm="12" md="4" lg="4">
+                <Paper
+                  elevation={6}
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 3,
+                  }}
+                >
+                  <Typography variant="h6" gutterBottom>
+                    Kevin Ryo Pratama
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    21120119130098
+                  </Typography>
+                </Paper>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
       </Container>
     </>
   );
